b76f65f702a36f07c461814cd562ca4d54f006b0
DylGun <78462130+DylGun@users.noreply.github.com>
2023-02-22 17:18:01 -0800

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from PIL import Image
+import PIL.ImageOps
+
+classes=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,random_state=9,)
+
+def get_prediction():
+    
